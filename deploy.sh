8cb1a6912f18e74815d19abd7a5df3f6b9d0a689
makosonm <makosonm@github.com>
2020-12-01 00:13:05 +0800

    Tue Dec  1 00:13:03 CST 2020

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,13 @@
 main() {
     DATE=`date`
     find . -name .DS_Store  -exec rm -f '{}' \;
+    git pull origin main
     git add --all
     git commit -m"${DATE}"
+    git push https://makosonm@github.com/makosonm/notes.git
+
+    git checkout master
+    git merge main
     git push https://makosonm@github.com/makosonm/notes.git
 }
 
