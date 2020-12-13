b3e42108bd28f7908aba5464da5bdcf9221ff095
makosonm <71538151+makosonm@users.noreply.github.com>
2020-12-13 15:06:18 +0800

    Update install.sh

--- scripts/vim/install.sh
+++ scripts/vim/install.sh
@@ -2,5 +2,6 @@
 
 curl -fLo ~/.vim/vimrc --create-dirs https://raw.githubusercontent.com/makosonm/notes/master/scripts/vim/vimrc
 
-cd ~/.vim
-vim -es -u ~/.vim/vimrc -i NONE -c "PlugInstall" -c "qa"
+
+#vim -es -u ~/.vim/vimrc -i NONE -c "PlugInstall" -c "qa"
+vim +PluginInstall +qall
